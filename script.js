7c2790c0ce6eec24e84f65c2c5956b408aae6c58
Artem <artemborisencko@gmail.com>
2025-03-26 21:25:14 +0200

    Mobile vers.

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const search = document.querySelector(".header__menu-link.search");
-const cart = document.querySelector(".header__menu-link.cart")
+const cart = document.querySelector(".header__menu-link.cart");
 
-search.addEventListener("click", function(event) {
+search.addEventListener("click", function (event) {
   event.preventDefault();
   alert("I am search icon");
-})
+});
 
-cart.addEventListener("click", function(event) {
+cart.addEventListener("click", function (event) {
   event.preventDefault();
   alert("I am cart icon");
-})
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   const navbarItems = document.querySelectorAll(".main__navbar-item");
 
-  navbarItems.forEach(item => {
-      item.addEventListener("click", () => {
-          navbarItems.forEach(nav => nav.classList.remove("active"));
-          item.classList.add("active");
-      });
+  navbarItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      navbarItems.forEach((nav) => nav.classList.remove("active"));
+      item.classList.add("active");
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector(".mobile-menu-btn");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  toggleButton.addEventListener("click", function () {
+    mobileMenu.classList.toggle("active");
   });
 });
